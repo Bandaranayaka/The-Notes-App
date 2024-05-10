3d81d94c811364a6e4d4cfea46b843f3fd6a8e6a
Bandaranayaka <3lakshana1124@gmail.com>
2024-05-11 02:35:04 +0530

    Implement EditNoteFragment for editing and deleting notes
    - Created EditNoteFragment class to provide functionality for editing and deleting notes.
    - Inflated the fragment layout using FragmentEditNoteBinding.
    - Initialized NoteViewModel to access note data.
    - Retrieved the current note from navigation arguments using navArgs.
    - Pre-filled the EditText fields with the current note's title and description.
    - Implemented functionality to update the note when the save button is clicked.
    - Implemented functionality to delete the note when the delete menu item is selected.
    - Implemented deleteNote function to show a confirmation dialog before deleting the note.
    - Implemented onCreateMenu to inflate and set up the menu for editing and deleting the note.
    - Implemented onMenuItemSelected to handle menu item selection and trigger deleteNote function when the delete menu item is clicked.
    - Implemented onDestroy to release FragmentEditNoteBinding reference.

--- app/src/main/java/com/example/thenotesapp/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/example/thenotesapp/viewmodel/NoteViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.thenotesapp.viewmodel
 
 import android.app.Application
+import android.provider.ContactsContract
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.room.Query
 import com.example.thenotesapp.model.Note
 import com.example.thenotesapp.repository.NoteRepository
 import kotlinx.coroutines.launch
@@ -14,11 +14,11 @@ class NoteViewModel(app: Application, private val noteRepository: NoteRepository
         viewModelScope.launch {
             noteRepository.insertNote(note)
         }
-    fun deleteNote(note: Note) =
+    fun deleteNote(note: ContactsContract.CommonDataKinds.Note) =
         viewModelScope.launch {
             noteRepository.deleteNote(note)
         }
-    fun updateNote(note: Note) =
+    fun updateNote(note: ContactsContract.CommonDataKinds.Note) =
         viewModelScope.launch {
             noteRepository.updateNote(note)
         }
